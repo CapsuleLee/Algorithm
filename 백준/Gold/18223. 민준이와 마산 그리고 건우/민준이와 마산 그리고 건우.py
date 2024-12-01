580cad0ced779f5d0716ea69b3127ae8a42707b6
SangMin LEE <163146180+CapsuleLee@users.noreply.github.com>
2024-12-01 22:40:08 +0900

    [Gold IV] Title: 민준이와 마산 그리고 건우, Time: 72 ms, Memory: 36260 KB -BaekjoonHub

--- 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.py
+++ 백준/Gold/18223. 민준이와 마산 그리고 건우/민준이와 마산 그리고 건우.py
@@ -6,35 +6,30 @@ input = sys.stdin.readline
 v,e,p = map(int,input().split())
 graph=[[] for _ in range(v+1)]
 
-distance1=[int(1e9)]*(v+1)
-distance2=[int(1e9)]*(v+1)
-distance3=[int(1e9)]*(v+1)
-
 for _ in range(e):
     a,b,c=map(int,input().split())
     graph[a].append((b,c))
     graph[b].append((a,c))
 
-def dijkstra(start, end,distance):
+def dijkstra(start, end):
     heap=[]
+    distance=[int(1e9)]*(v+1)
     distance[start]=0
     heapq.heappush(heap,(0,start))
 
     while heap:
         dist, node = heapq.heappop(heap)
-        
-        if distance[node]<dist:
+        if distance[node] < dist:
             continue
         for nodeInfo, distInfo in graph[node]:
             cost = dist+distInfo
             if distance[nodeInfo] >cost:
                 distance[nodeInfo] = cost
                 heapq.heappush(heap,(cost,nodeInfo))
+
     return distance[end]
 
-result=dijkstra(1,v,distance1)
-
-if result == dijkstra(p,v,distance3) + dijkstra(1,p,distance2):
+if dijkstra(1,v) == dijkstra(p,v) + dijkstra(1,p):
     print("SAVE HIM")
 else:
     print("GOOD BYE")
