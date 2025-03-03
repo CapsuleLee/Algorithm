841d87834d0430ab7584778301c65cd07f8d0d1a
SangMin LEE <163146180+CapsuleLee@users.noreply.github.com>
2025-03-03 22:22:51 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = -1;
+    int sum =0;
+    sum = accumulate(numbers.begin(), numbers.end(),0);
+    answer = 45-sum;
+    
+    
+    return answer;
+}
