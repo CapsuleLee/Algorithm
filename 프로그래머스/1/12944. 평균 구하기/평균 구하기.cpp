d4a1309495f7f10accc0e8b6a1a89beedb724cd8
SangMin LEE <163146180+CapsuleLee@users.noreply.github.com>
2025-03-10 19:21:32 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    answer = accumulate(arr.begin(),arr.end(),double(0));
+    answer = answer/arr.size();
+    return answer;
+}
