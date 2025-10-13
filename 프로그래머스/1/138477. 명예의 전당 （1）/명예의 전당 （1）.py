56393615e5556caf4dd56b2b5788ae6d83b0721b
SangMin LEE <163146180+CapsuleLee@users.noreply.github.com>
2025-10-13 13:59:48 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.56 ms, Memory: 9.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution(k, score):
+    answer = []
+    result=[]
+    for i in score:
+        answer.append(i)
+        answer.sort()
+        if len(answer) > k:
+            answer.pop(0)
+        
+        result.append(answer[0])
+    
+    return result
